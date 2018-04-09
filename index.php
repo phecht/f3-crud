5cde273ef6e93b6a3a2ba19ec1db09ce0a12041c
Pete Hecht <peteehecht@gmail.com>
2018-04-09 06:20:36 -0600

    Fixing local vs deployed

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
-// db_dns=pgsql:host=ec2-54-243-54-6.compute-1.amazonaws.com;port=5432;dbname=
-// db_name=d8ih8v0uco62k8
-// db_user=pshnyysokpitwg
-// db_pass=4de36e80c262da3b793ea42d10de9f8c94031a049ecdb6ab773247ca69918121 
 
 
-$f3=require('lib/base.php');
-$dns = $f3->get('db_dns');
-$dname = $f3->get('db_name');
-$duser = $f3->get('db_user');
-$dpass= $f3->get('db_pass');
+require 'lib/base.php';
 
-echo $dns, $dname, $duser, $dpass;
-$f3->config('config/config.ini');
-$f3->config('config/routes.ini');
+$f3 = Base::instance();
+
+if ( file_exists( '.env2' ) ) {
+	$f3->config( '.env2' );
+} else {
+	$temp = getenv( 'db_dns' );
+	$f3->set( 'db_dns', $tmp );
+	$temp = getenv( 'db_name' );
+	$f3->set( 'db_name', $tmp );
+	$temp = getenv( 'db_pass' );
+	$f3->set( 'db_pass', $tmp );
+	$temp = getenv( 'db_user' );
+	$f3->set( 'db_user', $tmp );
+}
+
+
+$f3->config( 'config/config.ini' );
+$f3->config( 'config/routes.ini' );
 $f3->run();
-?>
