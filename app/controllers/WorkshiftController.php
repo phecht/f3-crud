ade0fb60b79e4a6c3d25a2695ad40818cf8e5d3f
Pete Hecht <peteehecht@gmail.com>
2018-04-30 08:22:45 -0600

    Fixed select on update of workshifts.

--- app/controllers/WorkshiftController.php
+++ app/controllers/WorkshiftController.php
@@ -8,8 +8,8 @@ class WorkShiftController extends Controller {
 		$user_rows = $this->db->exec( 'select id, name FROM users' );
 		$this->f3->set( 'user_rows', $user_rows );
 
-		$workshiftv = $this->db->exec( 
-			'select w.id, w.startwork, w.endwork, u.name from workshifts w left join users u ON(u.id = w.userid)' );
+		$workshiftv = $this->db->exec(
+		'select w.id, w.startwork, w.endwork, u.name from workshifts w left join users u ON(u.id = w.userid) order by w.startwork' );
 
 		/* 		$this->f3->set( 'workshifts', $workshift->all() ); */
 		$this->f3->set( 'workshifts', $workshiftv );
@@ -47,7 +47,7 @@ class WorkShiftController extends Controller {
 			$workshift->edit( $this->f3->get( 'POST.id' ) );
 			$this->f3->reroute( '/wssuccess/Updated' );
 		} else {
-			echo 'in update';
+
 			$workshift->getById( $this->f3->get( 'PARAMS.id' ) );
 			$user      = new User( $this->db );
 			$user_rows = $this->db->exec( 'select id, name FROM users' );
