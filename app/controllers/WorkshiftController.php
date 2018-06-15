a635390243a7ce9544a3658467247533d627e2e4
Pete Hecht <peteehecht@gmail.com>
2018-06-14 21:04:11 -0600

    Added locations and base constructor to  controller.  Also added basic copy functionality

--- app/controllers/WorkshiftController.php
+++ app/controllers/WorkshiftController.php
@@ -2,6 +2,12 @@
 
 class WorkShiftController extends Controller {
 
+	function __construct() {
+		parent::__construct();
+		$this->f3->set( 'locs', $this->locs() );
+
+	}
+
 	public function index() {
 		$workshift = new Workshift( $this->db );
 		$user      = new User( $this->db );
@@ -9,10 +15,12 @@ class WorkShiftController extends Controller {
 		$this->f3->set( 'user_rows', $user_rows );
 
 		$workshiftv = $this->db->exec(
-		'select w.id, w.startwork, w.endwork, u.name from workshifts w left join users u ON(u.id = w.userid) order by w.startwork' );
-
-		/* 		$this->f3->set( 'workshifts', $workshift->all() ); */
+			'select w.id, w.startwork, w.endwork, u.name, l.locname from workshifts w 
+			left join users u ON(u.id = w.userid) 
+			left join locs l ON (l.id = w.shiftloc) order by w.startwork');
 		$this->f3->set( 'workshifts', $workshiftv );
+
+
 		$this->f3->set( 'page_head', 'Workshift List' );
 		$this->f3->set( 'message', $this->f3->get( 'PARAMS.message' ) );
 		$this->f3->set( 'view', 'workshift/list.htm' );
@@ -47,7 +55,6 @@ class WorkShiftController extends Controller {
 			$workshift->edit( $this->f3->get( 'POST.id' ) );
 			$this->f3->reroute( '/wssuccess/Updated' );
 		} else {
-
 			$workshift->getById( $this->f3->get( 'PARAMS.id' ) );
 			$user      = new User( $this->db );
 			$user_rows = $this->db->exec( 'select id, name FROM users' );
@@ -67,5 +74,25 @@ class WorkShiftController extends Controller {
 		}
 
 		$this->f3->reroute( '/wssuccess/Workshift Deleted' );
+	}
+
+	public function copyIt() {
+		if ( $this->f3->exists( 'PARAMS.id' ) ) {
+			$workshift = new Workshift( $this->db );
+			$workshift->getById( $this->f3->get( 'PARAMS.id' ) );
+			$new_workshift = new Workshift( $this->db );
+			$source        = $workshift->cast();
+			unset( $source['id'] );
+			$new_workshift->copyfrom( $source );
+			$new_workshift->save();
+			$this->f3->set( 'page_head', 'Update Workshift' );
+			$this->f3->set( 'view', 'workshift/update.htm' );
+			$this->f3->set( 'type', 'workshift' );
+		}
+	}
+
+
+	private function locs() {
+		return $this->db->exec( 'select id, locname FROM locs ' );
 	}
 }
